861225590cb7b41ea6f0c020b92257bfb9b4506d
Jack Lee <jack.y.l.dev@gmail.com>
2024-02-07 22:27:56 -0800

    impl piping

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-mod json;
+use core::fmt;
 use std::io::{self, BufRead, IsTerminal, Write};
 
+mod json;
 pub use json::{parse_json, Json, ParseError};
 
 pub fn run() {
@@ -15,7 +16,43 @@ pub fn run() {
             let line = line.unwrap();
             let json = parse_json(&line).unwrap();
 
-            stdout.write_fmt(format_args!("{json:?}\n")).unwrap();
+            let mut log = Log::default();
+
+            for (key, value) in json.as_object().unwrap() {
+                match *key {
+                    "timestamp" => log.timestamp = value.as_str(),
+                    "level" => log.level = value.as_str(),
+                    "message" => log.message = value.as_str(),
+                    _ => {}
+                }
+            }
+
+            stdout.write_fmt(format_args!("{log}\n")).unwrap();
         }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Log<'a> {
+    pub timestamp: Option<&'a str>,
+    pub level: Option<&'a str>,
+    pub message: Option<&'a str>,
+}
+
+impl fmt::Display for Log<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(timestamp) = self.timestamp {
+            write!(f, "{} ", timestamp)?;
+        }
+
+        if let Some(level) = self.level {
+            write!(f, "{} ", level)?;
+        }
+
+        if let Some(message) = self.message {
+            write!(f, "{}", message)?;
+        }
+
+        Ok(())
     }
 }
