6badf2f30ed34ae5e02d079c5c3defd60ab0e785
Joshua Barr <jobarr@amazon.com>
2026-06-15 10:17:31 -0700

    feat: Add  option and cleanly handle broken pipes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::io;
+use std::io::Error;
+
 fn main() {
     if let Err(e) = jlf::run() {
+        if let Some(ioe) = e.root_cause().downcast_ref::<Error>() {
+            if ioe.kind() == io::ErrorKind::BrokenPipe {
+                // Exit cleanly if the pipe reader disconnected
+                std::process::exit(0);
+            }
+        }
         eprintln!("{e:?}");
     }
 }
